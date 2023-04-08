e2fe69c28d4597cc5eaf5a4d86b903ef5c95ac0c
JungHeeyun <122995391+JungHeeyun@users.noreply.github.com>
2023-04-09 00:37:55 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,386 +1,158 @@
 #include <iostream>
-#include <string>
-#include <vector>
-#include <cstdlib>
 #include <ctime>
+#include <cstdlib>
+#include <vector>
+#include <string>
 #include <fstream>
-#include <random>
+#include <chrono>
+#include <thread>
+
 using namespace std;
 
-// Define constants for game parameters
-const int MAX_SANITY = 100;
-const int MIN_SANITY = 0;
-const int NUM_ROOMS = 5;
-const int NUM_ITEMS = 2;
-const int NUM_GHOSTS = 3;
+// Function prototypes
+void help();
+bool playGame(int roomNumber);
+int generateSanityBoost();
+string getSanityBoostItem();
+void printSlow(string text, int delay);
 
-// Define struct for game objects
-struct Item {
-    string name;
-    string description;
-    bool isKey;
-    int count;
+class Exorcist {
+public:
+    Exorcist(int sanity) : sanity(sanity) {}
+
+    int getSanity() { return sanity; }
+    void increaseSanity(int value) { sanity += value; }
+    void decreaseSanity(int value) { sanity -= value; }
+    
+
+private:
+    int sanity;
 };
 
-
-struct Room {
-    string name;
-    string description;
-    vector<Item> items;
-    int ghostChance;
-    bool isLocked;
-};
-
-// Define functions for game mechanics
-void printIntro();
-void printRoom(Room currentRoom);
-void printInventory(vector<Item> inventory);
-void addItem(vector<Item>& inventory, Item newItem);
-void removeItem(vector<Item>& inventory, int itemIndex);
-bool hasItem(vector<Item> inventory, string itemName);
-void printSanity(int sanity);
-void adjustSanity(int& sanity, int adjustment);
-Room generateRandomRoom();
-void generateItems(vector<Item>& items);
-void generateGhosts(int& numGhosts);
-void saveGame(Room currentRoom, vector<Item> inventory, int sanity);
-void loadGame(Room& currentRoom, vector<Item>& inventory, int& sanity);
 
 int main() {
-    // Seed random number generator
     srand(time(0));
 
-       // Initialize game variables
-    vector<Room> allRooms;
-    for (int i = 0; i < NUM_ROOMS; i++) {
-        allRooms.push_back(generateRandomRoom());
-    }
+    const int maxSanity(100);
+    const int sanityLossPerFailure(20);
+    const int numRooms(5);
+    
+    Exorcist player(maxSanity);
+    int currentRoom = 1;
+    string currentRoom2;
+    string roomDescriptions[5] = {
+    "You enter a dark and cold room. The air is heavy, and you can feel an evil presence nearby.",
+    "As you proceed to the next room, the walls seem to be closing in. A sense of dread fills your heart.",
+    "You find yourself in a room filled with broken mirrors. The distorted reflections send chills down your spine.",
+    "The stench of decay fills this room. Shadows dance on the walls as if they have a life of their own.",
+    "You finally reach the last room, where the possessed person lies. Their body is twisted in an unnatural way, and their eyes are pitch black."
+    };
 
-    // Initialize game variables
-    Room currentRoom = generateRandomRoom();
-    vector<Item> inventory;
-    generateItems(inventory);
-    int sanity = MAX_SANITY;
-    bool gameRunning = true;
-    int numVisitedRooms = 0;
+    printSlow("::::::::::: :::    ::: ::::::::::      :::::::::: :::    :::  ::::::::  :::::::::   :::::::: ::::::::::: :::::::: ::::::::::: \n", 1);
+    printSlow("    :+:     :+:    :+: :+:             :+:        :+:    :+: :+:    :+: :+:    :+: :+:    :+:    :+:    :+:    :+:    :+:      \n", 1);
+    printSlow("    +:+     +:+    +:+ +:+             +:+         +:+  +:+  +:+    +:+ +:+    +:+ +:+           +:+    +:+           +:+           \n", 1);
+    printSlow("    +#+     +#++:++#++ +#++:++#        +#++:++#     +#++:+   +#+    +:+ +#++:++#:  +#+           +#+    +#++:++#++    +#+           \n", 1);
+    printSlow("    +#+     +#+    +#+ +#+             +#+         +#+  +#+  +#+    +#+ +#+    +#+ +#+           +#+           +#+    +#+       \n", 1);
+    printSlow("    #+#     #+#    #+# #+#             #+#        #+#    #+# #+#    #+# #+#    #+# #+#    #+#    #+#    #+#    #+#    #+#       \n", 1);
+    printSlow("    ###     ###    ### ##########      ########## ###    ###  ########  ###    ###  ######## ########### ########     ###  \n", 1);
+    printSlow("\n\n\n\n\n                                                   The Cursed House                             \n\n", 15);
 
-    // Print game intro
-    printIntro();
+    cout << "Type 'help' for instructions." << endl << endl;
 
-    // Game loop
-    while (gameRunning && numVisitedRooms < NUM_ROOMS) {
-        // Print current room and inventory
-        printRoom(currentRoom);
-        printInventory(inventory);
+    while (player.getSanity() > 0 && currentRoom <= numRooms) {
+        string userInput;
+        cout << roomDescriptions[currentRoom - 1] << endl;
+        if (currentRoom == 1) {currentRoom2 = "first";}
+        else if (currentRoom == 2) {currentRoom2 = "second";}
+        else if (currentRoom == 3) {currentRoom2 = "third";}
+        else if (currentRoom == 4) {currentRoom2 = "fourth";}
+        else if (currentRoom == 5) {currentRoom2 = "last";}
 
-        // Print sanity level
-        printSanity(sanity);
+        cout << "You are in the " << currentRoom2 << "room " << ". Type 'play' to start the game, 'help' for instructions, or 'quit' to exit." << endl;
+        cin >> userInput;
+        cout << endl;
 
-        // Prompt player for action
-        cout << "What do you want to do? ";
-        string input;
-        getline(cin, input);
+        if (userInput == "play") {
+            bool success = playGame(currentRoom);
 
-        // Handle player action
-        if (input == "help") {
-        cout << "\nHere is a list of available commands:\n" << endl;
-        cout << " - look: Look around the room and check for any hidden dangers or items." << endl;
-        cout << " - take [item name]: Pick up an item found in the room and add it to your inventory." << endl;
-        cout << " - use [item name]: Use an item from your inventory. Usage varies depending on the item." << endl;
-        cout << " - unlock: Attempt to unlock the door if you have the key in your inventory." << endl;
-        cout << " - quit: Exit the game at any time." << endl;
-        cout << "\nRemember to explore carefully and manage your sanity while trying to escape the haunted mansion.\n" << endl;
-        }
-
-        else if (input == "look") {
-            // Generate random ghost encounter
-            if (rand() % 100 < currentRoom.ghostChance) {
-                cout << "You hear a strange noise and feel a chill run down your spine." << endl;
-                int numGhosts = 0;
-                generateGhosts(numGhosts);
-                cout << "You turn around and see " << numGhosts << " ghostly figures floating toward you!" << endl;
-                adjustSanity(sanity, -20);
-            }
-            else {
-                cout << currentRoom.description << endl;
-            }
-        }
-        else if (input.substr(0, 4) == "take") {
-            string itemName = input.substr(5);
-            if (hasItem(currentRoom.items, itemName)) {
-                int itemIndex = -1;
-                for (int i = 0; i < currentRoom.items.size(); i++) {
-                    if (currentRoom.items[i].name == itemName) {
-                        itemIndex = i;
-                        break;
-                    }
+            if (success) {
+                cout << "Congratulations! You have successfully completed room " << currentRoom << "." << endl;
+                if (currentRoom < numRooms) {
+                    int sanityBoost = generateSanityBoost();
+                    string item = getSanityBoostItem();
+                    cout << "You found a " << item << "! Your sanity increased by " << sanityBoost << "." << endl;
+                    player.increaseSanity(sanityBoost);
+                    cout << "\nYour current sanity: " << player.getSanity() << ".\n" << endl;
                 }
-                addItem(inventory, currentRoom.items[itemIndex]);
-                currentRoom.items.erase(currentRoom.items.begin() + itemIndex);
-                cout << "You pick up the " << itemName << "." << endl;
-            }
-            else { cout << "There's no " << itemName << " here." << endl;
-            }
-        }
-        else if (input.substr(0, 3) == "use") {
-            string itemName = input.substr(4);
-            if (hasItem(inventory, itemName)) {
-                if (itemName == "key" && currentRoom.isLocked) {
-                    currentRoom.isLocked = false;
-                    cout << "You use the key to unlock the door." << endl;
-                }
-                else if (itemName == "flashlight") {
-                    cout << "You turn on the flashlight and look around the room." << endl;
-                    adjustSanity(sanity, -10);
-                }
-                else {
-                    cout << "You can't use the " << itemName << " here." << endl;
-                }
-            }
-            else {
-                cout << "You don't have a " << itemName << "." << endl;
-            }
-        }
-        else if (input == "unlock") {
-            if (currentRoom.isLocked) {
-                if (hasItem(inventory, "key")) {
-                    currentRoom.isLocked = false;
-                    cout << "You use the key to unlock the door." << endl;
-                }
-                else {
-                    cout << "The door is locked and you don't have a key." << endl;
-                }
-            }
-            else {
-                cout << "The door is already unlocked." << endl;
-            }
-        }
-        else if (input == "quit") {
-            gameRunning = false;
-            cout << "Thanks for playing!" << endl;
-        }
-        else {
-            cout << "\nI didn't understand that command. Type 'help' for a list of available commands.\n" << endl;
-        }
-
-        // Check if player has won
-        if (!currentRoom.isLocked && currentRoom.items.empty()) {
-            numVisitedRooms++;
-            if (numVisitedRooms == NUM_ROOMS) {
-                gameRunning = false;
-                cout << "Congratulations! You have escaped the haunted mansion!" << endl;
+                currentRoom++;
             } else {
-                cout << "You have found the key and unlocked the door! ";
-                cout << "You must escape from the " << NUM_ROOMS - numVisitedRooms << " remaining rooms." << endl;
-                currentRoom = generateRandomRoom();
-                generateItems(currentRoom.items);
+                player.decreaseSanity(sanityLossPerFailure);
+                cout << "You failed! Your sanity decreased by " << sanityLossPerFailure << ". Your current sanity: " << player.getSanity() << "." << endl;
             }
-        }
-
-        // Check if player has lost
-        if (sanity <= MIN_SANITY) {
-            gameRunning = false;
-            cout << "You have gone insane and cannot escape the haunted mansion. Game over." << endl;
+        } else if (userInput == "help") {
+            help();
+        } else if (userInput == "quit") {
+            break;
+        } else {
+            cout << "Invalid command." << endl;
         }
     }
 
-    // Save game
-    saveGame(currentRoom, inventory, sanity);
-
+    if (player.getSanity() <= 0) {
+        cout << "You have lost all your sanity! Game over." << endl;
+    } else if (currentRoom > numRooms) {
+        cout << "Congratulations! You have saved the possessed person and won the game !" << endl;
+    } else {
+        cout << "Goodbye!" << endl;
+    }
     return 0;
 }
 
-void printIntro() {
-    cout << "Welcome to the Haunted Mansion game!" << endl;
-    cout << "You wake up in a haunted mansion with no memory of how you got there." << endl;
-    cout << "You notice that the front door is locked, and you must find the key to escape." << endl;
-    cout << "You also see that there are some items lying around that could be useful." << endl;
-    cout << "Be careful, as there are ghosts lurking in the shadows!" << endl;
-    cout << endl;
+
+// Function definitions
+void help() {
+    cout << "\nIn this game, you are an exorcist attempting to rescue a possessed person from a cursed house with 5 rooms." << endl;
+    cout << "You must complete a game in each room to proceed to the next room." << endl;
+    cout << "If you fail a game, you lose 20 sanity. If your sanity reaches 0, you lose the game." << endl;
+    cout << "Randomly, you may find a sacred item that will restore some sanity." << endl;
+    cout << "If you complete all 5 rooms, you save the possessed person and win the game!\n" << endl;
 }
+bool playGame(int roomNumber) {
+    // Implement the game for each room here.
+    // This is just a simple example. You can replace it with more complex games.
+    int randomNumber = rand() % 100 + 1;
+    int userGuess;
+    int maxAttempts = 5;
+    cout << "Room " << roomNumber << " game: Guess a number between 1 and 100. You have " << maxAttempts << " attempts." << endl;
 
-void printRoom(Room currentRoom) {
-    cout << "You are in the " << currentRoom.name << "." << endl;
-    cout << currentRoom.description << endl;
-    if (!currentRoom.items.empty()) {
-        cout << "You see the following items in the room:" << endl;
-        for (int i = 0; i < currentRoom.items.size(); i++) {
-            cout << " - " << currentRoom.items[i].name;
-            if (currentRoom.items[i].isKey) {
-                cout << " (key)";
-            }
-            cout << endl;
-        }
-    }
-}
+    for (int attempt = 1; attempt <= maxAttempts; attempt++) {
+        cout << "Attempt " << attempt << ": ";
+        cin >> userGuess;
 
-void printInventory(vector<Item> inventory) {
-    if (inventory.empty()) {
-        cout << "You don't have any items." << endl;
-    }
-    else {
-        cout << "You have the following items in your inventory:" << endl;
-        for (int i = 0; i < inventory.size(); i++) {
-            cout << " - " << inventory[i].name << " (" << inventory[i].count << ")";
-            if (inventory[i].isKey) {
-                cout << " (key)";
-            }
-            cout << endl;
-        }
-    }
-}
-
-
-void addItem(vector<Item>& inventory, Item newItem) {
-    bool itemFound = false;
-    for (Item& item : inventory) {
-        if (item.name == newItem.name) {
-            item.count++;
-            itemFound = true;
-            break;
-        }
-    }
-    if (!itemFound) {
-        newItem.count = 1;
-        inventory.push_back(newItem);
-    }
-}
-
-
-void removeItem(vector<Item>& inventory, int itemIndex) {
-    inventory.erase(inventory.begin() + itemIndex);
-}
-
-bool hasItem(vector<Item> inventory, string itemName) {
-    for (int i = 0; i < inventory.size(); i++) {
-        if (inventory[i].name == itemName) {
+        if (userGuess == randomNumber) {
             return true;
+        } else if (userGuess < randomNumber) {
+            cout << "The voice whispers, 'Higher...' " << endl;
+        } else {
+            cout << "The voice whispers, 'Lower...' " << endl;
         }
     }
     return false;
 }
-
-void printSanity(int sanity) {
-    cout << "Your sanity level is " << sanity << "/100." << endl;
+int generateSanityBoost() {
+    int randomValue = rand() % 3;
+    return 10;
+}
+string getSanityBoostItem() {
+    vector<string> items = {"holy water", "necklace", "blessed candle"};
+    return items[rand() % items.size()];
 }
 
-void adjustSanity(int& sanity, int adjustment) {
-    sanity += adjustment;
-    if (sanity > MAX_SANITY) {
-        sanity = MAX_SANITY;
+void printSlow(string text, int delay) {
+    for (char c : text) {
+        cout << c;
+        this_thread::sleep_for(chrono::milliseconds(delay));
+        cout.flush();
     }
-    else if (sanity < MIN_SANITY) {
-        sanity = MIN_SANITY;
-        cout << "You have gone insane and cannot continue!" << endl;
-    }
 }
 
-Room generateRandomRoom() {
-    // Define possible rooms
-    string names[NUM_ROOMS] = {"kitchen", "bedroom", "bathroom", "attic", "basement"};
-    string descriptions[NUM_ROOMS] = {"a spacious room with a large counter and lots of cabinets", 
-                                      "a cozy room with a large bed and a nightstand", 
-                                      "a small room with a toilet and a shower", 
-                                      "a dusty room with old furniture and cobwebs", 
-                                      "a dimly lit room with storage shelves and a workbench"};
-    int ghostChances[NUM_ROOMS] = {10, 5, 2, 20, 30};
-    bool isLockeds[NUM_ROOMS] = {false, false, true, true, true};
-
-    // Generate random room index
-    int roomIndex = rand() % NUM_ROOMS;
-
-    // Create room object
-    Room newRoom;
-    newRoom.name = names[roomIndex];
-    newRoom.description = descriptions[roomIndex];
-    newRoom.ghostChance = ghostChances[roomIndex];
-    newRoom.isLocked = isLockeds[roomIndex];
-
-    // Return new room object
-    return newRoom;
-}
-
-void generateItems(vector<Item>& items) {
-    // Define possible items
-    string names[NUM_ITEMS] = {"flashlight", "book"};
-    string descriptions[NUM_ITEMS] = {"a small flashlight with a working battery",
-                                      "a thick book with strange symbols on the cover"};
-    bool isKeys[NUM_ITEMS] = {false, false};
-
-    // Clear input vector
-    items.clear();
-
-    // Add flashlight and book to inventory
-    Item flashlight = {names[0], descriptions[0], isKeys[0]};
-    Item book = {names[1], descriptions[1], isKeys[1]};
-    items.push_back(flashlight);
-    items.push_back(book);
-}
-
-
-void generateGhosts(int& numGhosts) {
-    // Generate random number of ghosts
-    numGhosts = rand() % 5 + 1;
-}
-
-void saveGame(Room currentRoom, vector<Item> inventory, int sanity) {
-    // Open file for writing
-    ofstream saveFile("save.txt");
-
-    // Write game state to file
-    saveFile << currentRoom.name << endl;
-    saveFile << currentRoom.description << endl;
-    saveFile << currentRoom.ghostChance << endl;
-    saveFile << currentRoom.isLocked << endl;
-    saveFile << inventory.size() << endl;
-    for (int i = 0; i < inventory.size(); i++) {
-        saveFile << inventory[i].name << endl;
-        saveFile << inventory[i].description << endl;
-        saveFile << inventory[i].isKey << endl;
-    }
-    saveFile << sanity << endl;
-
-    // Close file
-    saveFile.close();
-}
-
-void loadGame(Room& currentRoom, vector<Item>& inventory, int& sanity) {
-    // Open the file for reading
-    ifstream file("saved_game.txt");
-    if (!file.is_open()) {
-        cout << "Error: Could not open file." << endl;
-        return;
-    }
-
-    // Read the data from the file
-    string roomName, roomDescription;
-    int ghostChance, isLocked, numItems, numGhosts;
-    file >> roomName >> ws;
-    getline(file, roomDescription);
-    file >> ghostChance >> isLocked >> numItems >> numGhosts;
-
-    // Set the values of the current room
-    currentRoom.name = roomName;
-    currentRoom.description = roomDescription;
-    currentRoom.ghostChance = ghostChance;
-    currentRoom.isLocked = (bool)isLocked;
-
-    // Clear the items vector and read in the new items
-    inventory.clear();
-    for (int i = 0; i < numItems; i++) {
-        string itemName, itemDescription;
-        bool isKey;
-        file >> itemName >> ws;
-        getline(file, itemDescription);
-        file >> isKey;
-        Item newItem = { itemName, itemDescription, isKey };
-        inventory.push_back(newItem);
-    }
-
-    // Read in the sanity level
-    file >> sanity;
-
-    // Close the file
-    file.close();
-}
-// tired
